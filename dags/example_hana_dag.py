8f2a32e5ebc8721ddb4e9290b487f973a76e881f
Jonathan Takish <jonathan.takish@gmail.com>
2026-06-26 12:04:18 -0400

    Version 3.0
    - Bump version to 3.0
    - Update the example_hana_dag.py to reflect the new changes
    - Add `Faker` as a dependency under the `example_dag` group

--- dags/example_hana_dag.py
+++ dags/example_hana_dag.py
@@ -1,16 +1,14 @@
 from __future__ import annotations
 
 import csv
-
+from textwrap import dedent
 from faker import Faker
 from faker.providers import automotive, person
 from pendulum import datetime
 
 from airflow.providers.common.sql.decorators.sql import sql_task
-from airflow.providers.common.sql.operators.sql import BranchSQLOperator
-from airflow.providers.standard.operators.empty import EmptyOperator
-from airflow.sdk import dag, task
-from airflow_provider_sap_hana.hooks.hana import SapHanaHook
+from airflow_provider_sap_hana.operators.hana import SapHanaInsertRowsOperator
+from airflow.sdk import dag, task, get_current_context, ObjectStoragePath
 
 
 @dag(
@@ -21,99 +19,90 @@ from airflow_provider_sap_hana.hooks.hana import SapHanaHook
     catchup=False,
 )
 def example_hana_dag():
-    check_table_exists = BranchSQLOperator(
-        task_id="check_table_exists",
-        conn_id="hana_default",
-        follow_task_ids_if_false=["create_table"],
-        follow_task_ids_if_true=["do_nothing"],
-        sql="""
-        SELECT COUNT(*)
-        FROM sys.tables
-        WHERE
-            schema_name = 'AIRFLOW'
-            AND table_name = 'FAKE_VEHICLE_REGISTRATIONS';""",
-    )
 
-    @sql_task(conn_id="hana_default")
-    def create_table():
-        return """
-        CREATE TABLE airflow.fake_vehicle_registrations (
-            vin NVARCHAR(17),
-            owner_name_first NVARCHAR(30),
-            owner_name_last NVARCHAR(30),
-            address NVARCHAR(100),
-            city NVARCHAR(30),
-            state NVARCHAR(2),
-            postal_code NVARCHAR(20),
-            country NVARCHAR(2),
-            created_at TIMESTAMP,
-            PRIMARY KEY (vin)
-          );"""
-
-    do_nothing = EmptyOperator(task_id="do_nothing")
-
-    @task(trigger_rule="none_failed_min_one_success")
+    @task
     def create_fake_data():
         fake = Faker()
         fake.add_provider(automotive)
         fake.add_provider(person)
 
-        with open("/tmp/fake_data.csv", mode="w", encoding="utf-8") as f:
+        base = ObjectStoragePath("/tmp/fake_data.csv")
+
+        with base.open(mode="w", encoding="utf-8") as f:
             writer = csv.writer(f)
             for _ in range(1000000):
-                vin = fake.vin()
-                owner_name_first = fake.first_name().upper()
-                owner_name_last = fake.last_name().upper()
-                street = fake.street_address().upper()
-                city = fake.city().upper()
-                state = fake.state_abbr().upper()
-                postal_code = fake.postalcode().upper()
-                country = "US"
-                created_at = fake.date_time_this_decade().isoformat()
                 writer.writerow(
                     (
-                        vin,
-                        owner_name_first,
-                        owner_name_last,
-                        street,
-                        city,
-                        state,
-                        postal_code,
-                        country,
-                        created_at,
+                        fake.vin(),
+                        fake.first_name().upper(),
+                        fake.last_name().upper(),
+                        fake.street_address().upper(),
+                        fake.city().upper(),
+                        fake.state_abbr().upper(),
+                        fake.postalcode().upper(),
+                        "US",
+                        fake.date_time_this_decade().isoformat(),
                     )
                 )
+        return base
 
-    @task(trigger_rule="none_failed_min_one_success")
-    def insert_into_hana():
-        hook = SapHanaHook(enable_db_log_messages=True)
+    @task
+    def insert_into_hana(tmp_file: ObjectStoragePath, **kwargs):
 
-        with open("/tmp/fake_data.csv", encoding="utf-8") as f:
-            rows = csv.reader(f)
-            hook.bulk_insert_rows(
-                table="airflow.fake_vehicle_registrations",
-                rows=rows,
-                commit_every=100000,
-                replace=True,
-                autocommit=True,
-            )
-        hook.get_db_log_messages()
+        ctx = get_current_context()
+        task_id = ctx["task"].task_id
+
+        with tmp_file.open(mode="r", encoding="utf-8") as f:
+            data = csv.reader(f)
+            _ = next(data)
+            rows = list(data)
+
+        create_stmt = dedent("""
+            DO
+            BEGIN
+                DECLARE tableExists TINYINT := 0;
+
+                SELECT COUNT(*) INTO tableExists
+                FROM sys.tables
+                WHERE
+                    schema_name = 'AIRFLOW'
+                    AND table_name = 'FAKE_VEHICLE_REGISTRATIONS';
+
+                IF tableExists = 0 THEN EXEC
+                    'CREATE TABLE airflow.fake_vehicle_registrations (
+                        vin NVARCHAR(17),
+                        owner_name_first NVARCHAR(30),
+                        owner_name_last NVARCHAR(30),
+                        address NVARCHAR(100),
+                        city NVARCHAR(30),
+                        state NVARCHAR(2),
+                        postal_code NVARCHAR(20),
+                        country NVARCHAR(2),
+                        created_at TIMESTAMP,
+                        PRIMARY KEY (vin)
+                      )';
+                END IF;
+            END;""")
+        operator = SapHanaInsertRowsOperator(
+            task_id=task_id,
+            conn_id="hana_default",
+            table_name="fake_vehicle_registrations",
+            schema="airflow",
+            rows=rows,
+            insert_args={"replace": True, "commit_every": 10000},
+            preoperator=create_stmt,
+        )
+        operator.execute(ctx)
 
     @sql_task(conn_id="hana_default")
     def get_rows():
-        return """
-        SELECT *
-        FROM airflow.fake_vehicle_registrations
-        LIMIT 100;
-        """
+        stmt = dedent("""
+            SELECT *
+            FROM airflow.fake_vehicle_registrations
+            LIMIT 100;""")
+        return stmt
 
-    (
-        check_table_exists
-        >> [create_table(), do_nothing]
-        >> create_fake_data()
-        >> insert_into_hana()
-        >> get_rows()
-    )
+    insert_into_hana(create_fake_data()) >> get_rows()
 
 
 example_hana_dag()
--- src/airflow_provider_sap_hana/__init__.py
+++ src/airflow_provider_sap_hana/__init__.py
@@ -4,7 +4,7 @@ import packaging.version
 
 from airflow import __version__ as airflow_version
 
-__version__ = "2.1"
+__version__ = "3.0"
 
 __all__ = ["__version__"]
 
